2113a1cd4c6e72873e4db6085c4796e5913728ae
rajen <rajen@rajen.info>
2017-03-20 12:42:00 +0000

    del footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="row site-info">
-			<?php echo '&copy; '.date("Y"); ?> 
+			<!--<?php echo '&copy; '.date("Y"); ?>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Proudly Powered by ','nisarg')); ?>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nisarg' ) ); ?>">WordPress</a>
@@ -22,7 +22,7 @@
 			<?php 
 			$nisarg_theme_author_str =  '<a href="'.esc_url('http://www.falgunidesai.com/').'" rel="designer">Falguni Desai</a>'; 
 			printf( esc_html__( 'Theme: %1$s by %2$s.', 'nisarg' ), 'Nisarg',$nisarg_theme_author_str); 
-			?>
+			?>-->
 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
