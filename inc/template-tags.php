9cc9cdf816f6168d987a76a7993d102340c9c331
rajen <rajen@rajen.info>
2017-03-20 13:15:47 +0000

    del tags:author

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -120,17 +120,13 @@ function nisarg_posted_on() {
 
 $viewbyauthor_text = __( 'View all posts by', 'nisarg' ).' %s';
 
-$entry_meta = '<i class="fa fa-calendar-o"></i> <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s </time></a><span class="byline"><span class="sep"></span><i class="fa fa-user"></i>
-<span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>';
+$entry_meta = '<i class="fa fa-calendar-o"></i> <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s </time></a><span class="byline"><span class="sep"></span></span>&nbsp';
 
 	$entry_meta = sprintf($entry_meta,
 		esc_url( get_permalink() ),
         esc_attr( get_the_time() ),
         esc_attr( get_the_date( 'c' ) ),
-        esc_html( get_the_date() ),
-        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-        esc_attr( sprintf( $viewbyauthor_text, get_the_author() ) ),
-        esc_html( get_the_author() ));
+        esc_html( get_the_date() ));
 
     print $entry_meta;
 
